c9ded9f441233ce4902404aae2d75d59defd62a3
MarcoVacchi <mvacchi816@gmail.com>
2025-04-01 12:36:40 +0200

    feat: complete not found

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const router = require("./router/postRouter");
+const notFound = require("./middlewares/notFound");
 
 app.use(express.static("public"));
 
 app.use(express.json());
 
-
-
 app.use("/postRouter", router);
+
+app.use(notFound);
 
 //console.log("ciao");
 
